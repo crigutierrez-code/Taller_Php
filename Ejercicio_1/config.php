235e997aed91e6ef8c75e91d1e54c4a69fe8fae0
Edward Pasachoa <eypasachoa@uniboyac.edu.co>
2025-09-21 17:48:26 +0200

    punto 1 y parte del 3

--- Ejercicio_1/config.php
+++ Ejercicio_1/config.php
@@ -1,3 +1,12 @@
 <?php
-
+$number = explode(",", $_POST$_POST["numero"]); 
+$valor = $number % 2;
+ echo"<ul>";
+for($i = 1; $i <= count($number); $i++){
+     if ($valor == 0){ 
+        echo "<li>" $number[$i] "es par </li> ";
+    }else{ 
+        echo "<li>" $number[$i] "es impar </li>"; 
+    } 
+} echo"</ul>" 
 ?>
--- Ejercicio_3/config.php
+++ Ejercicio_3/config.php
@@ -1,3 +1,17 @@
 <?php
+$number = explode(",", $_POST$_POST["numero"]); 
+echo"Promedio";
+for($i = 1; $i <= count($number); $i++){
+    $suma = $number[$i-1] + $number[$i]; 
+}
+$Pro = $suma/ count($number);
+echo "el promedio de los datos es:" . $Pro
 
+echo"media";
+$med = count($number)% 2;
+echo "la media de los datos es :".$number[$med];
+
+ echo"moda";
+for($i = 1; $i <= count($number); $i++){ 
+}
 ?>
