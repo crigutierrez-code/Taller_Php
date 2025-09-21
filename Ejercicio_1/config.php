103f7dcf4ac778e206d2a64e51d8b7a82cd19a05
Edward Pasachoa <eypasachoa@uniboyac.edu.co>
2025-09-21 18:17:59 +0200

    terminar punto 3

--- Ejercicio_1/config.php
+++ Ejercicio_1/config.php
@@ -1,12 +1,12 @@
 <?php
-$number = explode(",", $_POST$_POST["numero"]); 
-$valor = $number % 2;
+$number = explode(",", $_POST["numero"]); 
  echo"<ul>";
 for($i = 1; $i <= count($number); $i++){
+    $valor = $number[$i] % 2;
      if ($valor == 0){ 
-        echo "<li>" $number[$i] "es par </li> ";
+        echo "<li>" . $number[$i] . "es par </li> ";
     }else{ 
-        echo "<li>" $number[$i] "es impar </li>"; 
+        echo "<li>" . $number[$i] . "es impar </li>"; 
     } 
 } echo"</ul>" 
 ?>
--- Ejercicio_3/config.php
+++ Ejercicio_3/config.php
@@ -1,17 +1,27 @@
 <?php
-$number = explode(",", $_POST$_POST["numero"]); 
+$number = explode(",", $_POST["numero"]); 
 echo"Promedio";
 for($i = 1; $i <= count($number); $i++){
     $suma = $number[$i-1] + $number[$i]; 
 }
 $Pro = $suma/ count($number);
-echo "el promedio de los datos es:" . $Pro
+echo "el promedio de los datos es:" . $Pro;
 
 echo"media";
 $med = count($number)% 2;
 echo "la media de los datos es :".$number[$med];
 
  echo"moda";
-for($i = 1; $i <= count($number); $i++){ 
+ $moda1=0; 
+for($j = 1; $j <= count($number); $j++){
+for($i = 1; $i >= count($number); $i++){
+    if ($number[$j]== $number[$i]){
+        $moda++;
+        if($moda >$moda1){
+            $modaN = $number[$i];
+        }
+    }
+
+}
 }
 ?>
