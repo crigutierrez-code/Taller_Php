1f7c1a83657c8a23aaf30235ea2effd924f09846
Edward Pasachoa <eypasachoa@uniboyac.edu.co>
2025-09-22 17:26:28 +0200

    terminar el punto 3 y correcion de bugs

--- Ejercicio_3/config.php
+++ Ejercicio_3/config.php
@@ -1,27 +1,43 @@
 <?php
 $number = explode(",", $_POST["numero"]); 
-echo"Promedio";
-for($i = 1; $i <= count($number); $i++){
-    $suma = $number[$i-1] + $number[$i]; 
+
+echo "La lista de datos dada es: " . implode(", ", $number) . "<br>";
+
+echo"Promedio <br>";
+$suma = 0;
+foreach($number as $valor){
+    $suma += floatval(trim($valor));
 }
 $Pro = $suma/ count($number);
-echo "el promedio de los datos es:" . $Pro;
+echo "el promedio de los datos es:" . $Pro ."<br>";
 
-echo"media";
-$med = count($number)% 2;
-echo "la media de los datos es :".$number[$med];
+echo"media <br>";
+sort($number);
+$med = count($number);
+if ($med % 2== 0){
+    $n1=$number[$med/2 -1] ;
+    $n2=$number[$med/2 ];
+    $mediana = ($n1 + $n2) / 2;
+}else{
+    $mediana = $number[floor($med/2)];
+}
+echo "la media es : $mediana <br>";
 
- echo"moda";
- $moda1=0; 
-for($j = 1; $j <= count($number); $j++){
-for($i = 1; $i >= count($number); $i++){
+
+ echo"moda <br>";
+$modaN = null;
+$moda1=0; 
+for($j = 0; $j < count($number); $j++){
+    $moda =0;
+for($i = 0; $i < count($number); $i++){
     if ($number[$j]== $number[$i]){
         $moda++;
-        if($moda >$moda1){
-            $modaN = $number[$i];
-        }
     }
-
 }
+    if($moda >$moda1){
+        $moda1 = $moda;
+        $modaN = $number[$j];
+    }
 }
+echo "la moda es:". $modaN;
 ?>
