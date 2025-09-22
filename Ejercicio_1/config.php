be7db0916b86de80f2be4431e2c87c12fb964ade
Edward Pasachoa <eypasachoa@uniboyac.edu.co>
2025-09-22 15:09:48 +0200

    se soluciono el erro con los numeos decimales string y cero

--- Ejercicio_1/config.php
+++ Ejercicio_1/config.php
@@ -1,12 +1,16 @@
 <?php
-$number = explode(",", $_POST["numero"]); 
+$number = explode(",", $_POST["numero"]);
  echo"<ul>";
-for($i = 1; $i <= count($number); $i++){
-    $valor = $number[$i] % 2;
-     if ($valor == 0){ 
-        echo "<li>" . $number[$i] . "es par </li> ";
-    }else{ 
-        echo "<li>" . $number[$i] . "es impar </li>"; 
-    } 
-} echo"</ul>" 
+for($i = 0; $i < count($number); $i++){
+     $valor = trim($number[$i]);
+    if (filter_var($valor, FILTER_VALIDATE_INT) !== false){ 
+            if ($valor%2 == 0){ 
+                    echo "<li>" . $number[$i] . ":es número par </li> ";
+            }else{ 
+                    echo "<li>" . $number[$i] . ": es número impar </li>"; 
+            } 
+    }else{
+            echo "<li>" . $number[$i] .": no es un número entero";
+    }
+}
 ?>
