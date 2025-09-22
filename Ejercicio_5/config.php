56ec8d981ab136ce149494363287ac7f2e1915a3
Edward Pasachoa <eypasachoa@uniboyac.edu.co>
2025-09-22 17:53:30 +0200

    terminar el punto 5 y se revisaron bugs

--- Ejercicio_5/config.php
+++ Ejercicio_5/config.php
@@ -1,3 +1,15 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $buscar = trim($_POST["buscar"]);
+    $parrafo = trim($_POST["texto"]);
 
+    $resultado = str_ireplace($buscar, "<mark>$buscar</mark>", $parrafo);
+
+    echo "<h3>Texto original:</h3>";
+    echo "<p>$parrafo</p>";
+    echo "<h3>Se busca:<h3>";
+    echo "<p>$buscar</p>";
+    echo "<h3>Texto con coincidencias resaltadas:</h3>";
+    echo "<p>$resultado</p>";
+}
 ?>
