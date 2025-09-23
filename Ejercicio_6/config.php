6556fe5172410cc5284c75964709e25a4d6e90d5
crigutierrez-code <crigutierrez@uniboyaca.edu.co>
2025-09-22 21:44:24 -0500

    Adelanto del ejercicio 6 sin css

--- Ejercicio_6/config.php
+++ Ejercicio_6/config.php
@@ -1,3 +1,11 @@
+<!-- archivo: divisible.php -->
 <?php
+$a = $_POST["a"];
+$b = $_POST["b"];
 
+if ($a % $b == 0) {
+    echo "El número $a es divisible entre el número $b";
+} else {
+    echo "El número $a no es divisible entre el número $b";
+}
 ?>
